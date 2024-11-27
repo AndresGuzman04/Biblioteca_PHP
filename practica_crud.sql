aac7878d4b3f5266a7f650dbeebaca05c95caa37
AndresGuzman04 <andr35guzman2001@gmail.com>
2024-11-26 21:22:37 -0600

    practica_crud.sql modified

--- practica_crud.sql
+++ practica_crud.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-06-2023 a las 07:56:13
--- Versión del servidor: 10.4.17-MariaDB
--- Versión de PHP: 7.2.34
+-- Tiempo de generación: 27-11-2024 a las 04:20:21
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,14 +33,7 @@ CREATE TABLE `alquiler` (
   `id_cliente` int(11) NOT NULL,
   `fecha` date NOT NULL,
   `accion` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `alquiler`
---
-
-INSERT INTO `alquiler` (`id`, `id_libro`, `id_cliente`, `fecha`, `accion`) VALUES
-(3, 5, 9, '2023-06-05', 'Entrada');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -53,21 +46,7 @@ CREATE TABLE `clientes` (
   `nombre` varchar(100) NOT NULL,
   `direccion` text NOT NULL,
   `telefono` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `clientes`
---
-
-INSERT INTO `clientes` (`id`, `nombre`, `direccion`, `telefono`) VALUES
-(1, 'Andres', 'Ciudad Barrios', 70990101),
-(2, 'Andres', 'Ciudad Barrios', 70990101),
-(3, 'refre', 'Ciudad Barrios', 656),
-(5, 'cece', 'cec', 0),
-(6, 'frfr', 'frefer', 2147483647),
-(7, 'Andres', 'Ciudad Barrios', 1),
-(8, '44', '434', 2147483647),
-(9, 'Erika', 'Ciudad Barrios', 78666666);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -79,17 +58,7 @@ CREATE TABLE `libros` (
   `id` int(11) NOT NULL,
   `nombre` varchar(255) NOT NULL,
   `imagen` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `libros`
---
-
-INSERT INTO `libros` (`id`, `nombre`, `imagen`) VALUES
-(3, 'Ejemplo1', '1685669938_Captura3.PNG'),
-(4, 'vvvvvv', '1685671686_Captura2.PNG'),
-(5, 'gg', '1685664194_Clase UML.jpeg'),
-(11, 'refre', '1685662559_Captura4.PNG');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -101,7 +70,7 @@ CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `usuario` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
